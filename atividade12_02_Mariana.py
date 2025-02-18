73078ec493b4af7fae3a4ec5f59b41ce347b2a70
Mariana Macena de Magalh�es <56463939811@>
2025-02-18 15:35:41 -0300

    Add existing file

---
+++ atividade12_02_Mariana.py
@@ -0,0 +1,30 @@
+# Operações Aritméticas (+, -, *, /)
+# Divisão inteira (//) -  mostrar o resutado inteiro.
+# Resto da Divisão (%) - usado para obter o resto da divisão
+# Potencia (**) - elevar um números á potencia
+
+# exercicio 1
+y = 10.8
+x = 15.5
+z = 9.9
+
+media = y + x + z/3
+
+print(media)
+
+# exercicios com input
+#y = int(input("Qual sua 1° nota? "))
+#x = int(input("Qual sua 2° nota? "))
+#z = int(input("Qual sua 3° nota? "))
+
+#media = y + x + z/3
+
+#print(media)
+
+# exercicio 2
+prest = int(input("Qual o valor da sua prestação? "))
+juros = 10/100
+
+valorTotal = prest + juros/12
+
+print(f"O valor á ser pago é de ",valorTotal)
