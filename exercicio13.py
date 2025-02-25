25d3a35e04f4a19028bd691086fa8ca21b369a21
MarianaMagalh <jorgegameplay811@gmail.com>
2025-02-25 14:38:11 -0300

    adição do exercicio 13

---
+++ exercicio13.py
@@ -0,0 +1,9 @@
+# exercicio 13
+# calculo da potência de um número inteiro elevado a um expoente
+print("Pontência!")
+num = int(input("Insirá um número: "))
+expo = int(input("Insirá o expoente desse número: "))
+
+potencia = num ** expo
+
+print(f"A potência do {num} elevado à {expo} é {potencia}.")
