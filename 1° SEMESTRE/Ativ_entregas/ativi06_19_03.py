b43dcc8b0f2bae8801512ba1ee8d61fffc5d45ab
56463939811 <jorgegameplay811@gmail.com>
2025-03-19 16:57:39 -0300

    nova atividade. Dia 19/03

--- 1° SEMESTRE/Ativ_entregas/ativi06_19_03.py
+++ 1° SEMESTRE/Ativ_entregas/ativi06_19_03.py
@@ -1,12 +1,14 @@
 # ex 1
+#print("Ano Bissexto!")
 #ano = int(input("digite o ano: "))
 
 #if ano // 4 == 0 :
    # print(f"O {ano} é bissexto")
 #else :
    # print(f"O {ano} é bissexto")
-
+#print("-------------------------------------------------------------------------------------------")
 # ex 2
+#print("IMC!")
 '''
 peso = float(input("Insira o seu peso: "))
 altu = float(input("Insira a sua altura:"))
@@ -27,8 +29,9 @@ else:
     print(f"Seu IMC é {imc:,.2f}, logo sua classificação é obesidade extrema.")
 
 '''
-
+#print("-------------------------------------------------------------------------------------------")
 # ex 3
+#print("Desconto de Produto!")
 '''
 qtdprodutos = int(input("Insira a quantidade de produtos a ser pago: "))
 valorprodutos = float(input("insira o valor dos produtos:"))
@@ -39,6 +42,57 @@ if qtdprodutos <= 100 :
     print(f"Valor Original: {valorprodutos}\nQuantidade de Produtos: {qtdprodutos}\nDesconto por unidade: {desconto}\nValor Final: {}")
 
 '''
+#print("-------------------------------------------------------------------------------------------")
 
 # ex 4
+#print("Voto Obrigatório!")
+'''
 idade = int(input("Insira a sua idade: "))
+
+if idade < 18:
+    print("Voto Obrigatorio")
+elif idade >= 16 and idade <= 18 and idade >= 70:
+    print("Voto Facultativo")
+else:
+    print("Voto Não Eleitor")
+'''
+
+#print("-------------------------------------------------------------------------------------------")
+
+# ex 5
+'''
+print("Maior e Menor")
+
+idaps1 = int(input("Insira a sua idade pessoa 1: "))
+idaps2 = int(input("Insira a sua idade pessoa 2: "))
+idaps3 = int(input("Insira a sua idade pessoa 3: "))
+
+if idaps1 > idaps2 and idaps1 > idaps3:
+    print("A pessoa 1 tem a maior idade.")
+    if idaps2 > idaps3:
+        print("A pessoa 3 da idade menor idade")
+    else:
+        print("A pessoa 2 tem a menor idade")
+
+elif idaps2 > idaps3 and idaps2 > idaps1:
+    print("A pessoa 2 tem a maior idade.")
+    if idaps1 >  idaps3:
+        print("A pessoa 3 tem a menor idade")
+    else:
+        print("a pessoa 1 tem a menor idade")
+else:
+    print("A pessoa 3 tem a maior idade")
+    if idaps1 > idaps2:
+        print("A pessoa 2 tem a menor idade")
+    else:
+        print("A pessoa 1 tem a menor idade")
+'''
+#print("-------------------------------------------------------------------------------------------")
+hora = int(input("Digite a hora: "))
+min = int(input("Digite os minutos: "))
+seg = int(input("digite os segundo: "))
+
+if hora >= 24 and hora < 0 and min >= 60 and seg >= seg:
+    print("Horario Invalida!")
+else:
+    print("Horario Valida!")
