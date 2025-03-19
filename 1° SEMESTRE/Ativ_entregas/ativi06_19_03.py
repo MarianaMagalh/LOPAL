db54dbf80070b51b645a159541c886677d875503
56463939811 <jorgegameplay811@gmail.com>
2025-03-19 14:12:31 -0300

    nova atividade. Dia 19/03

---
+++ 1° SEMESTRE/Ativ_entregas/ativi06_19_03.py
@@ -0,0 +1,44 @@
+# ex 1
+#ano = int(input("digite o ano: "))
+
+#if ano // 4 == 0 :
+   # print(f"O {ano} é bissexto")
+#else :
+   # print(f"O {ano} é bissexto")
+
+# ex 2
+'''
+peso = float(input("Insira o seu peso: "))
+altu = float(input("Insira a sua altura:"))
+
+imc = peso / (altu ** 2)
+
+if imc <= 18.5 :
+    print(f"Seu IMC é {imc:,.2f}, logo sua classificação é baixo peso.")
+elif imc >= 18.5 and imc <= 24.4 :
+    print(f"Seu IMC é {imc:,.4f}, logo sua classificação é peso normal.")
+elif imc >= 25 and imc <= 29.9:
+    print(f"Seu IMC é {imc:,.2f}, logo sua classificação é sobrepeso.")
+elif imc >= 30 and imc <= 34.9:
+    print(f"Seu IMC é {imc:,.2f}, logo sua classificação é obesidade.")
+elif imc >= 35 and imc <= 39.9:
+    print(f"Seu IMC é {imc:,.42f}, logo sua classificação é obesidade mórbida.")
+else:
+    print(f"Seu IMC é {imc:,.2f}, logo sua classificação é obesidade extrema.")
+
+'''
+
+# ex 3
+'''
+qtdprodutos = int(input("Insira a quantidade de produtos a ser pago: "))
+valorprodutos = float(input("insira o valor dos produtos:"))
+
+if qtdprodutos <= 100 :
+    desconto = valorprodutos * (10/100)
+    total = 
+    print(f"Valor Original: {valorprodutos}\nQuantidade de Produtos: {qtdprodutos}\nDesconto por unidade: {desconto}\nValor Final: {}")
+
+'''
+
+# ex 4
+idade = int(input("Insira a sua idade: "))
