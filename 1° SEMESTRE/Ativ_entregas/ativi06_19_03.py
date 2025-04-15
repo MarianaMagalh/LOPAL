80337ee8ab55f0000b5fbe7fa58650c0f5a18234
Mariana Macena <167040224+MarianaMagalh@users.noreply.github.com>
2025-04-15 04:53:21 -0300

    Update ativi06_19_03.py

--- 1° SEMESTRE/Ativ_entregas/ativi06_19_03.py
+++ 1° SEMESTRE/Ativ_entregas/ativi06_19_03.py
@@ -1,204 +1,300 @@
-# ex 1
-#print("Ano Bissexto!")
-#ano = int(input("digite o ano: "))
-
-#if ano // 4 == 0 :
-   # print(f"O {ano} é bissexto")
-#else :
-   # print(f"O {ano} é bissexto")
-#print("-------------------------------------------------------------------------------------------")
-# ex 2
-#print("IMC!")
+# exer 1
 '''
-peso = float(input("Insira o seu peso: "))
-altu = float(input("Insira a sua altura:"))
+contador = 0
 
-imc = peso / (altu ** 2)
-
-if imc <= 18.5 :
-    print(f"Seu IMC é {imc:,.2f}, logo sua classificação é baixo peso.")
-elif imc >= 18.5 and imc <= 24.4 :
-    print(f"Seu IMC é {imc:,.4f}, logo sua classificação é peso normal.")
-elif imc >= 25 and imc <= 29.9:
-    print(f"Seu IMC é {imc:,.2f}, logo sua classificação é sobrepeso.")
-elif imc >= 30 and imc <= 34.9:
-    print(f"Seu IMC é {imc:,.2f}, logo sua classificação é obesidade.")
-elif imc >= 35 and imc <= 39.9:
-    print(f"Seu IMC é {imc:,.42f}, logo sua classificação é obesidade mórbida.")
-else:
-    print(f"Seu IMC é {imc:,.2f}, logo sua classificação é obesidade extrema.")
-
-'''
-#print("-------------------------------------------------------------------------------------------")
-# ex 3
-#print("Desconto de Produto!")
-'''
-qtdprodutos = int(input("Insira a quantidade de produtos a ser pago: "))
-valorprodutos = float(input("insira o valor dos produtos:"))
-
-if qtdprodutos >= 100 :
-    desconto = valorprodutos * (10/100)
-    total = valorprodutos - desconto
-    print(f"Valor Original: {valorprodutos}\nQuantidade de Produtos: {qtdprodutos}\nDesconto por unidade: {desconto}\nValor Final: {total}")
-elif qtdprodutos < 100:
-    desconto = valorprodutos * (5/100)
-    total = valorprodutos - desconto
-    print(f"Valor Original: {valorprodutos}\nQuantidade de Produtos: {qtdprodutos}\nDesconto por unidade: {desconto}\nValor Final: {total}")
-else:
-    print("Valores invalidos!")
-'''
-#print("-------------------------------------------------------------------------------------------")
-
-# ex 4
-#print("Voto Obrigatório!")
-'''
-idade = int(input("Insira a sua idade: "))
-
-if idade >= 18 and idade <= 60:
-    print("Voto obrigatorio")
-elif idade <= 16 and idade < 18 or idade > 70:
-    print("Voto Facultativo")
-else:
-    print("Não eleitor")
-'''
-
-#print("-------------------------------------------------------------------------------------------")
-
-# ex 5
-'''
-print("Maior e Menor")
-
-idaps1 = int(input("Insira a sua idade pessoa 1: "))
-idaps2 = int(input("Insira a sua idade pessoa 2: "))
-idaps3 = int(input("Insira a sua idade pessoa 3: "))
-
-if idaps1 > idaps2 and idaps1 > idaps3:
-    print("A pessoa 1 tem a maior idade.")
-    if idaps2 > idaps3:
-        print("A pessoa 3 da idade menor idade")
+for num in range(11):
+    resposta = int(input("Digite um número: "))
+    if resposta % 3 == 0:
+        print(f"O número {resposta} é multiplo ")
+        contador += 1
     else:
-        print("A pessoa 2 tem a menor idade")
+        print(f"Número {resposta} não é multiplo")
+print(f"a quatidade dos números multiplos de 3: {contador}")
+'''
 
-elif idaps2 > idaps3 and idaps2 > idaps1:
-    print("A pessoa 2 tem a maior idade.")
-    if idaps1 >  idaps3:
-        print("A pessoa 3 tem a menor idade")
+
+
+
+# exer 2
+'''
+print("Tentativas de Senhas")
+while True:
+    senha = input("Digite a 'senha correta' correta: ")
+    if senha == 'senha correta':
+        break
+'''
+
+# exer 3
+'''
+while True:
+    print("Menu:")
+    print("sorvete")
+    print("bolo")
+    print("torta de morango")
+    print("mousse de limão")
+    print("bombom de morango")
+    print("sair")
+
+    op = input("Escolha uma das opções acima: ")
+
+    if op == 'sair':
+        break
+'''
+
+# exer 4
+'''
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite um número: "))
+
+comeco = min(num1, num2)
+final = max(num1, num2)
+
+for i in range(comeco + 1, final):
+    primo = True
+
+    for j in range(2, i):
+        if i % j == 0:
+            primo = False
+            break
+    if primo:
+        print(f"{i} é primo")
+'''
+
+
+# exer 5
+'''
+senha = '1234'
+
+contador = 0
+
+while contador < 4:
+    senhaofc = input("Insira a sua senha: ")
+    if senha == senhaofc:
+        print("Seja bem vinde!")
+        break
     else:
-        print("a pessoa 1 tem a menor idade")
-else:
-    print("A pessoa 3 tem a maior idade")
-    if idaps1 > idaps2:
-        print("A pessoa 2 tem a menor idade")
+        contador += 1
+        if contador == 3:
+            print("Acesso bloqueado")
+            break
+'''
+
+
+# exer 6
+'''
+pares = []
+impares = []
+
+for num in range(10):
+    numeros = int(input("Insira um numero: "))
+
+    if numeros % 2 == 0:
+        pares.append(numeros)
     else:
-        print("A pessoa 1 tem a menor idade")
-'''
-#print("-------------------------------------------------------------------------------------------")
-'''
-print("Validação do Horario!")
-hora = int(input("Digite a hora: "))
-min = int(input("Digite os minutos: "))
-seg = int(input("Digite os segundo: "))
+        impares.append(numeros)
 
-if hora >= 24 and hora < 0 and min >= 60 and seg >= seg:
-    print("Horario Invalida!")
+
+print(f"Lista de pares\n{pares}")
+print(f"Lista de impares\n{impares}")
+'''
+
+# exer 7
+'''
+frase = input("Insira um frase: ")
+frase = frase.lower()
+
+conA = 0
+conE = 0
+conI = 0
+conO = 0
+conU = 0
+
+for letra in frase:
+    if letra == 'a':
+        conA += 1
+    elif letra == 'e':
+        conE += 1
+    elif letra == 'i':
+        conI += 1
+    elif letra == 'o':
+        conO += 1
+    elif letra == 'u':
+        conU += 1
+    
+if conA == 0 and conE == 0 and conI == 0 and conO == 0 and conU == 0:
+    print("Não há vogais nessa palavra/frase")
 else:
-    print("Horario Valida!")
+    print(f"Quantidade de vogais na palavra/frase {frase}")
+    print(f"A: {conA}\nE: {conE}\nI: {conI}\nO: {conO}\nU: {conU}")
 '''
-#print("-------------------------------------------------------------------------------------------")
-'''
-print("Conversao de variaveis")
-notanum = int(input("Insira a nota do aluno: "))
 
-if notanum <= 0 and notanum >= 10:
-    print("Nota invalida!\nInsira novamente.")
-else:
-    print("Nota valida!")
-
-notastg = str(notanum)
-
-if notastg >= '9' and notastg == '10':
-    print("A")
-elif notastg >= '7' and notastg < '9':
-    print("B")
-elif notastg >= '5' and notastg < '7':
-    print("C")
-elif notastg >= '3' and notastg < '5':
-    print("D")
-else:
-    print("E")
+# exer 8
 '''
-#print("-------------------------------------------------------------------------------------------")
-'''
-print("Quadrado ou retagulo?")
-la1 = int(input("Insira o primeiro lado: "))
-la2 = int(input("Insira o segundo lado: "))
-la3 = int(input("Insira o treceiro lado: "))
-la4 = int(input("Insira o quarto lado: "))
-
-if la1 == la2 and la1 == la3 and la1 == la4:
-    print("Quadrado!")
-else:
-    print("Retagulo!")
-'''
-#print("-------------------------------------------------------------------------------------------")
-'''
-print("Calculadora")
-num1 = float(input("Digite o primeiro valor: "))
-num2 = float(input("Digite o segundo valor: "))
-
-op = input("Qual operação vc irá utilizar?\n+, -, *, /")
-
-if op == '+':
-    soma = num1 + num2
-    print(f"Resultado: {soma}")
-elif op == '-':
-    sub = num1 - num2
-    print(f"Resultado: {sub}")
-elif op == '*':
-    multi = num1 * num2
-    print(f"Resultado: {multi}")
-else:
-    div = num1 / num2
-    print(f"Resultado: {div}")
-'''
-#print("-------------------------------------------------------------------------------------------")
-'''
-print("Calculadora de Média")
-nota1 = int(input("Insira a primeira nota: "))
-nota2 = int(input("Insira a segunda nota: "))
-nota3 = int(input("Insira a terceira nota: "))
-
-if nota1 < nota2 and nota1 < nota3:
-    media = (nota2 + nota3) / 2
-    print(f"Media: {media}")
-elif nota2 < nota1 and nota2 < nota3:
-    media = (nota1 + nota3) / 2
-    print:(f"Media: {media}")
-else:
-    media = (nota1 + nota2) / 2
-    print(f"Media: {media}")
-'''
-#print("-------------------------------------------------------------------------------------------")
-# desafio
 import random
-# escolha de um numero aleatorio
-numsecreto = random.randint(1, 10)
 
-sugestao1 = int(input("Adivinhe o número entre 1 e 10: "))
+contCara = 0
 
-if sugestao1 == numsecreto:
-    print("Parabéns! Voce acertou!")
-else:
-    if sugestao1 < numsecreto:
-        print("O número secreto é maior")
+while contCara < 3:
+    moeda = random.choice(['cara', 'coroa'])
+    print(moeda)
+    if moeda == 'cara':
+        contCara += 1
     else:
-        print("O número secreto é menor")
+        contCara = 0
+'''
+
+# exer 9
+'''
+lisNum = []
+
+while True:
+    numeros = input("Insira um número: ")
+    if numeros == 'sair':
+        break
+    else:
+        numero = int(numeros)
+        lisNum.append(numero)
+
+media = sum(lisNum) / len(lisNum)
+
+contMenos = 0
+
+for num in lisNum:
+    if num < media:
+        contMenos += 1
+
+print(f"A media: {media}\nNúmero que são menores do a media: {contMenos}")
+'''
 
 
-sugestao2 = int(input("Tente mais uma vez: "))
+# exer 10
+'''
+sequencia = []
 
-if sugestao2 == numsecreto:
-    print("Parabéns! Você acertou na segunda tentativa!")
+while True:
+    numeros = input("Insira um número: ")
+    if numeros == 'sair':
+        break
+    else:
+        numero = int(numeros)
+        sequencia.append(numero)
+
+if len(sequencia) < 2:
+    print("Impossivel realizar a procura")
 else:
-    print(f"Que pena! O número correto era {numsecreto}.")
+    sequencia = list(set(sequencia))
+    sequencia.sort()
+    segMaior = sequencia[1]
+    print(f"O segundo maior é {segMaior}")
+'''
+
+# exer 11
+'''
+a = 1
+b = 2
+c = 0
+
+for i in range(1, 6):
+    if i % 2 == 0:
+        a += i
+    else:
+        b *= i
+    c = a + b
+print(a, b, c)
+'''
+
+
+# exer 12
+'''
+x = 3
+y = 2
+z = 0
+
+while x < 8:
+    for j in range(2):
+        z += y
+        y += 1
+    x += 1
+
+print(x, y, z)
+'''
+
+
+# Desafio 1
+'''
+popuCoelho = int(input("Insira a quantidade de coelho: "))
+geração = int(input("Ate quantas gerações vc deseja ir: "))
+
+while True:
+    taxaRepro = float(input("Insira a taxa de reprodução: ex: 0.2 "))
+    taxaMor = float(input("Insira a taxa de mortalidade: ex: 0.1"))
+
+    gera = 0
+    while gera < geração:
+        popuCoelho = popuCoelho * (1 + taxaRepro - taxaMor)
+        gera += 1
+
+        print(f"--- Geração: {gera}° --- População: {popuCoelho:.1f}")
+        print(f"--- Taxa Reprodução: {taxaRepro} --- Taxa Mortalidade: {taxaMor}")
+
+    break
+'''
+
+# Desafio 2
+'''
+import random
+
+palavras = ['abacaxi', 'banana', 'morango', 'melancia', 'uva', 'laranja']
+palavra = random.choice(palavras)
+
+
+oculta = ["_" for _ in palavra]
+
+letErradas = []
+letCertas = []
+tentativas = 6
+
+print(" Jogo da Forca ")
+
+while True:
+    print("\nPalavra: ", " ".join(oculta))
+    print("Letras erradas:", ", ".join(letErradas))
+    print(f"Tentativas restantes: {tentativas}")
+
+    letra = input("Digite uma letra: ").lower()
+
+    if not letra.isalpha() or len(letra) != 1:
+        print("Digite apenas uma letra.")
+        continue
+
+    if letra in letCertas or letra in letErradas:
+        print("Você já tentou essa letra. Tente outra.")
+        continue
+
+    if letra in palavra:
+        letCertas.append(letra)
+        for i in range(len(palavra)):
+            if palavra[i] == letra:
+                oculta[i] = letra
+        print("Letra correta!")
+    else:
+        letErradas.append(letra)
+        tentativas -= 1
+        print("Letra errada!")
+
+    if "_" not in oculta:
+        print("\nParabéns! Você ganhou!")
+        print("A palavra era:", palavra)
+        break
+
+    if tentativas == 0:
+        print("\nVocê perdeu!")
+        print("A palavra era:", palavra)
+        break
+'''
+
+
+
+
+
 
